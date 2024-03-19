9a70c160a83b581aca4a3e4f37bbdb2c1b2ccbea
trey-rosius <treyrosius@gmail.com>
2024-03-19 12:08:15 +0100

    Initial commit

---
+++ test/social_api_cdk_rust.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SocialApiCdkRust from '../lib/social_api_cdk_rust-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/social_api_cdk_rust-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SocialApiCdkRust.SocialApiCdkRustStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
